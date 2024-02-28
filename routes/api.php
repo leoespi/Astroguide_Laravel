395c252a309cc2320228404aaca4925491819e32
leoespi <riveraleo113@gmail.com>
2024-02-28 09:58:57 -0500

    agogo

--- routes/api.php
+++ routes/api.php
@@ -49,3 +49,4 @@ Route::get('/test', function () {
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
 Route::post('quiz/validarTerminacion', [QuizApiController::class, 'validarTerminacion'])->middleware('auth:api');
+Route::get('/logroUser',[LogroApiController::class, 'logroUser'])->middleware('auth:api');
